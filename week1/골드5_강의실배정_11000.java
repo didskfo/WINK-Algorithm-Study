c2f522adb2a0e9ba6b37903c2463edb4b01fb025
didskfo <101495452+didskfo@users.noreply.github.com>
2025-01-15 21:52:20 +0900

    Update 골드5_강의실배정_11000.java

--- week1/골드5_강의실배정_11000.java
+++ week1/골드5_강의실배정_11000.java
@@ -10,10 +10,7 @@ public class 골드5_강의실배정_11000 {
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-        PriorityQueue<int[]> que = new PriorityQueue<>(((q1, q2) -> {
-            if (q1[0] != q2[0]) return q1[0] - q2[0];
-            return q1[1] - q2[1];
-        }));
+        PriorityQueue<int[]> que = new PriorityQueue<>((q1, q2) -> Integer.compare(q1[0], q2[0]));
         StringTokenizer st;
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine(), " ");
@@ -22,14 +19,14 @@ public class 골드5_강의실배정_11000 {
             que.offer(new int[]{s, t});
         }
 
+        // 강의 종료 시간 우선순위 큐
         PriorityQueue<Integer> pq = new PriorityQueue<>();
-        pq.offer(0);
         while (!que.isEmpty()) {
             int[] cur = que.poll();
-            if (cur[0] >= pq.peek()) pq.poll();
-            pq.offer(cur[1]);
+            if (!pq.isEmpty() && cur[0] >= pq.peek()) pq.poll(); // 앞 강의 종료시간보다 현 강의 시작시간이 뒤라면 강의실 재사용 가능
+            pq.offer(cur[1]); // 현 강의 종료시간 추가
         }
-        
+
         System.out.println(pq.size());
     }
 }
