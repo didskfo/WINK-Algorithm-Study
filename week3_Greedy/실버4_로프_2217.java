ec0837b39da6f7c1a0f690c5d3975af0fad793ee
didskfo <101495452+didskfo@users.noreply.github.com>
2025-02-12 13:56:05 +0900

    Update 실버4_로프_2217.java
    주석 추가

--- week3_Greedy/실버4_로프_2217.java
+++ week3_Greedy/실버4_로프_2217.java
@@ -13,11 +13,11 @@ public class 실버4_로프_2217 {
             for (int i = 0; i < N; i++) {
                 arr[i] = Integer.parseInt(br.readLine());
             }
-            Arrays.sort(arr);
+            Arrays.sort(arr); // 오름차순 정렬 -> for문에서 반대 순서로 확인
     
             int maxWeight = 0;
             for (int i = 0; i < N; i++) {
-                if (maxWeight < arr[N-i-1]*(i+1)) {
+                if (maxWeight < arr[N-i-1]*(i+1)) { // 현재 중량을 들어올릴 경우가 최대 중량일 때
                     maxWeight = arr[N-i-1]*(i+1);
                 } 
             }
